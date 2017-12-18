7e7082b8c010cd5b7035b7869a0420a24a31c461
Dave Cozens <davecozens.defra@gmail.com>
2017-12-18 11:16:38 +0000

    resolve db-migrate issue

--- migration/migrations/20171218084708-setup-wab-team.js
+++ migration/migrations/20171218084708-setup-wab-team.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20171218084707-setup-wab-team-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20171218084708-setup-wab-team-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20171218084707-setup-wab-team-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20171218084708-setup-wab-team-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
