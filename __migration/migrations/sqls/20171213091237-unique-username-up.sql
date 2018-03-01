ff8dfbb72598da637a9e75de72fcbe3abc3e2561
James Carmichael <james@siteclick.co.uk>
2018-03-01 10:31:08 +0000

    Tweak migrations

--- __migration/migrations/sqls/20171213091237-unique-username-up.sql
+++
@@ -1,4 +0,0 @@
-/* Replace with your SQL commands */
-
-ALTER TABLE idm.users
-    ADD CONSTRAINT user_name_unique UNIQUE (user_name)
--- __migration/migrations/sqls/20171214130749-fix-timestamp-login-up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "idm"."users" ADD COLUMN IF NOT EXISTS last_login date;
-
-ALTER TABLE "idm"."users"
-  ALTER COLUMN "last_login" TYPE timestamp(0) USING "last_login"::timestamp(0);
