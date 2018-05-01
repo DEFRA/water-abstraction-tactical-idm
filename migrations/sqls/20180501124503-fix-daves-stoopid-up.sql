39cf461616c202fbd5e205d7cbe601c9d3862370
Dave Cozens <davecozens@users.noreply.github.com>
2018-05-01 14:01:50 +0100

    Update 20180501124503-fix-daves-stoopid-up.sql

--- migrations/sqls/20180501124503-fix-daves-stoopid-up.sql
+++ migrations/sqls/20180501124503-fix-daves-stoopid-up.sql
@@ -1,37 +1,43 @@
 DROP VIEW if exists "idm"."kpi_view";
-CREATE VIEW "idm"."kpi_view" AS
-SELECT 'additional_access_grantees'::text AS datapoint,
-   count(*) AS value,'Users granted access by another user' as description
-  FROM crm.entity_roles
- WHERE (entity_roles.created_by IS NOT NULL)
+ SELECT (x.usertype || '_registrations_completed'::text) AS datapoint,
+    count(*) AS value,
+    (('Registrations completed post public beta for '::text || x.usertype) || ' users'::text) AS description
+   FROM ( SELECT
+                CASE
+                    WHEN ((to_jsonb((users.user_data)::json) ->> 'usertype'::text) IS NOT NULL) THEN (to_jsonb((users.user_data)::json) ->> 'usertype'::text)
+                    ELSE 'external'::text
+                END AS usertype
+           FROM idm.users
+          WHERE ((users.last_login IS NOT NULL) AND (users.user_id > 113))) x
+  GROUP BY (x.usertype || '_registrations_completed'::text), x.usertype
 UNION
-SELECT 'renamed_licences'::text AS datapoint,
-   count(*) AS value, 'Licences given a custom name (includes Severn Trent)' as description
-  FROM crm.document_header
- WHERE ((document_header.document_name IS NOT NULL)
- -- AND ((document_header.company_entity_id)::text <> '4b4a645f-a3ce-7827-e118-50a45ca7c1d1'::text)
- )
+ SELECT (x.usertype || '_registrations_not_completed'::text) AS datapoint,
+    count(*) AS value,
+    (('Registrations started post public beta but not completed for '::text || x.usertype) || ' users'::text) AS description
+   FROM ( SELECT
+                CASE
+                    WHEN ((to_jsonb((users.user_data)::json) ->> 'usertype'::text) IS NOT NULL) THEN (to_jsonb((users.user_data)::json) ->> 'usertype'::text)
+                    ELSE 'external'::text
+                END AS usertype
+           FROM idm.users
+          WHERE ((users.last_login IS NULL) AND (users.user_id > 113))) x
+  GROUP BY (x.usertype || '_registrations_not_completed'::text), x.usertype
 UNION
-SELECT 'users_with_multiple_verifications'::text AS datapoint,
-   count(*) AS value, 'Users who have verified multiple batches of licences' as description
-  FROM ( SELECT verification.entity_id,
-           count(*) AS count
-          FROM crm.verification
-         GROUP BY verification.entity_id
-        HAVING (count(*) > 1)) x
-union
-SELECT 'verifications_started_this_month'::text AS datapoint,
-   count(*) AS value, 'Verification flows started this calender month' as description
-   from crm.verification where date_trunc('month', current_date) = date_trunc('month', date_created)
-union
-SELECT 'verifications_started_last_month'::text AS datapoint,
-   count(*) AS value, 'Verification flows started last calender month' as description
-   from crm.verification where date_trunc('month', current_date - interval '1' month) = date_trunc('month', date_created)
-union
-SELECT 'verifications_completed_this_month'::text AS datapoint,
-   count(*) AS value, 'Verification flows completed this calender month' as description
-   from crm.verification where date_trunc('month', current_date) = date_trunc('month', date_verified)
-union
-SELECT 'verifications_completed_last_month'::text AS datapoint,
-   count(*) AS value, 'Verification flows completed last calender month' as description
-   from crm.verification where date_trunc('month', current_date - interval '1' month) = date_trunc('month', date_verified)
+ SELECT (x.usertype || '_user_accounts'::text) AS datapoint,
+    count(*) AS value,
+    (('Total '::text || x.usertype) || '  post public beta user accounts'::text) AS description
+   FROM ( SELECT
+                CASE
+                    WHEN ((to_jsonb((users.user_data)::json) ->> 'usertype'::text) IS NOT NULL) THEN (to_jsonb((users.user_data)::json) ->> 'usertype'::text)
+                    ELSE 'external'::text
+                END AS usertype
+           FROM idm.users
+          WHERE (users.user_id > 113)) x
+  GROUP BY (x.usertype || '_user_accounts'::text), x.usertype
+UNION
+ SELECT 'total_user_accounts'::text AS datapoint,
+    count(*) AS value,
+    'Total post public beta user accounts'::text AS description
+   FROM idm.users
+  WHERE (users.user_id > 113)
+  GROUP BY 'total_user_accounts'::text
