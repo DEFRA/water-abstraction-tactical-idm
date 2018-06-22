1027bd3dccc70d21acfa813f687da97af0a40d72
Mark Swaffer <mdswaffer@gmail.com>
2018-06-22 13:37:22 +0100

    WATER-1180: Update to HAPI 17
     - Lints to standard spec
     - Adds codeclimate config to exclude certain directories
     - Moves config out to separate file.
     - Updates Hapi to v17
     - Updates Hapi plugins to be compatible with Hapi v17
     - Removes redundant UI asset generation code (gulp)

--- src/lib/idm.js
+++ src/lib/idm.js
@@ -1,29 +1,27 @@
-const Helpers = require('./helpers')
-const DB = require('./connectors/db')
-const Notify = require('./connectors/notify')
-const Slack = require('./slack')
+const Helpers = require('./helpers');
+const DB = require('./connectors/db');
+const Notify = require('./connectors/notify');
+const uuidv4 = require('uuid/v4');
 
-
-function loginError(request, reply) {
-  reply({
+function loginError (request, h) {
+  return h.response({
     user_id: null,
     err: 'Unknown user name or password'
-  }).code(401)
+  }).code(401);
 }
 
 /**
  * Error class for if user not found
  */
 class UserNotFoundError extends Error {
-  constructor(message) {
+  constructor (message) {
     super(message);
     this.name = 'UserNotFoundError';
   }
-
 }
 
 class NotifyError extends Error {
-  constructor(message) {
+  constructor (message) {
     super(message);
     this.name = 'NotifyError';
   }
@@ -41,176 +39,157 @@ class NotifyError extends Error {
  * @param {String} [request.params.sender] - email address of the sender, sharing only
  * @param {String} request.query.mode - mode
  */
-async function reset(request, reply) {
+async function reset (request, h) {
   const mode = request.query.mode || 'reset';
   const sender = request.query.sender || null;
   const {email} = request.params;
-  const resetGuid = Helpers.createGUID();
-
+  const resetGuid = uuidv4();
 
   // Locate user
   // @TODO hapi-pg-rest-api would be cleaner if hapi-pg-rest-api exposed DB interaction layer
   try {
-      const query = `SELECT * FROM idm.users WHERE LOWER(user_name)=LOWER($1)`;
-      const {err, data} = await DB.query(query, [email]);
-      if(err) {
-        throw err;
+    const query = `SELECT * FROM idm.users WHERE LOWER(user_name)=LOWER($1)`;
+    const { err, data } = await DB.query(query, [email]);
+    if (err) {
+      throw err;
+    }
+    if (data.length !== 1) {
+      throw new UserNotFoundError();
+    }
+
+    // Update user with reset guid
+    const query2 = `UPDATE idm.users SET reset_guid=$1 WHERE user_id=$2`;
+    const { err: err2 } = await DB.query(query2, [resetGuid, data[0].user_id]);
+    if (err2) {
+      throw err2;
+    }
+
+    // Send password reset email
+    const userData = JSON.parse(data[0].user_data || '{}');
+    const result = await Notify.sendPasswordResetEmail({
+      email,
+      firstName: userData.firstname || '(User)',
+      resetGuid,
+      sender
+    }, mode);
+
+    if (result.error) {
+      throw new NotifyError(result.error);
+    }
+
+    // Success
+    return {
+      error: null,
+      data: {
+        user_id: data[0].user_id,
+        user_name: data[0].user_name,
+        reset_guid: resetGuid
       }
-      if(data.length !== 1) {
-        throw new UserNotFoundError();
-      }
-
-      // Update user with reset guid
-      const query2 = `UPDATE idm.users SET reset_guid=$1 WHERE user_id=$2`;
-      const {err : err2} = await DB.query(query2, [resetGuid, data[0].user_id]);
-      if(err2) {
-        throw err2;
-      }
-
-      // Send password reset email
-      const userData = JSON.parse(data[0].user_data || '{}');
-      const result = await Notify.sendPasswordResetEmail({
-        email,
-        firstName : userData.firstname || '(User)',
-        resetGuid,
-        sender
-      }, mode);
-
-      if(result.error) {
-        throw new NotifyError(result.error);
-      }
-
-      // Success
-      return reply({
-        error : null,
-        data : {
-          user_id : data[0].user_id,
-          user_name : data[0].user_name,
-          reset_guid : resetGuid
-        }
-      });
-
-  }
-  catch(error) {
-    if(error.name === 'UserNotFoundError') {
-      return reply({data : null, error}).code(404);
+    };
+  } catch (error) {
+    if (error.name === 'UserNotFoundError') {
+      return h.response({data: null, error}).code(404);
     }
 
     console.log(error);
 
-    reply({
-      data : null,
+    return h.response({
+      data: null,
       error
     }).code(500);
   }
-
 }
 
-
-
-function loginUser(request, reply) {
-
-  doUserLogin (request.payload.user_name,request.payload.password,0).then((result)=>{
-    return reply(result)
-  }).catch(()=>{
-    return loginError(request, reply)
-  })
-
+function loginUser (request, h) {
+  return doUserLogin(request.payload.user_name, request.payload.password, 0)
+    .then((result) => {
+      return result;
+    }).catch(() => {
+      return loginError(request, h);
+    });
 }
 
-function loginAdminUser(request, reply) {
-  console.log(`Received admin login user request for ${request.payload.user_name}`)
-  doUserLogin (request.payload.user_name,request.payload.password,0).then((result)=>{
-    return reply(result)
-  }).catch(()=>{
-    return loginError(request, reply)
-  })
+function loginAdminUser (request, h) {
+  console.log(`Received admin login user request for ${request.payload.user_name}`);
+  return doUserLogin(request.payload.user_name, request.payload.password, 0)
+    .then((result) => {
+      return result;
+    }).catch(() => {
+      return loginError(request, h);
+    });
 }
 
-function doUserLogin(user_name,password,admin){
+function doUserLogin (user_name, password, admin) {
   return new Promise((resolve, reject) => {
-    if(admin){
-      var query = `select * from idm.users where lower(user_name)=lower($1) and admin=1`
-    } else {
-      var query = `select * from idm.users where lower(user_name)=lower($1)`
-    }
-    var queryParams = [user_name]
-    // console.log(query)
-    // console.log(queryParams)
+    const query = admin
+      ? `select * from idm.users where lower(user_name)=lower($1) and admin=1`
+      : `select * from idm.users where lower(user_name)=lower($1)`;
+
+    const queryParams = [user_name];
+
     DB.query(query, queryParams)
       .then((UserRes) => {
-        //admin login query result
+        // admin login query result
         if (UserRes.data[0]) {
           Helpers.compareHash(password, UserRes.data[0].password).then(() => {
-            resetLockCount(UserRes.data[0]).then(()=>{
-              var query = `select split_part(user_name,'@',1)||'...' as id, last_login from idm.users order by last_login desc`
-              DB.query(query).then((res)=>{
-                var logins=`* ${process.env.NODEENV} Login History:*`
-                for (r in res.data){
-                  logins+=`\n ${res.data[r].id} ${res.data[r].last_login}`
-                }
-                DB.query('update idm.users set last_login = current_date where user_id=$1', [UserRes.data[0].user_id]).then((res,err)=>{
+            resetLockCount(UserRes.data[0]).then(() => {
+              var query = `select split_part(user_name,'@',1)||'...' as id, last_login from idm.users order by last_login desc`;
+              DB.query(query).then((res) => {
+                DB.query('update idm.users set last_login = current_date where user_id=$1', [UserRes.data[0].user_id]).then((res, err) => {
                 })
-                .catch(()=>{})
-                .then(()=>{
+                .catch(() => {})
+                .then(() => {
                   resolve({
                     user_id: UserRes.data[0].user_id,
                     err: null,
                     reset_required: UserRes.data[0].reset_required,
                     reset_guid: UserRes.data[0].reset_guid,
-                    last_login:UserRes.data[0].last_login,
-                    bad_logins:UserRes.data[0].bad_logins,
-                    user_data:UserRes.data[0].user_data
-                  })
-                })
-
-              }).catch((err)=>{
-                console.log(err)
-                reject()
-              })
-
-//            })
-            })
-
+                    last_login: UserRes.data[0].last_login,
+                    bad_logins: UserRes.data[0].bad_logins,
+                    user_data: UserRes.data[0].user_data
+                  });
+                });
+              }).catch((err) => {
+                console.log(err);
+                reject();
+              });
+            });
           }).catch(() => {
 //            console.log('rejected for incorect hash')
-            increaseLockCount(UserRes.data[0]).then(()=>{
-              console.log('Incorrect hash')
-              reject('Incorrect hash')
-            }).catch(()=>{
-              console.log('Incorrect hash & notify email failed')
-              reject('Incorrect hash & notify email failed')
-            })
+            increaseLockCount(UserRes.data[0]).then(() => {
+              console.log('Incorrect hash');
+              reject('Incorrect hash');
+            }).catch(() => {
+              console.log('Incorrect hash & notify email failed');
+              reject('Incorrect hash & notify email failed');
+            });
           });
         } else {
-          console.log('rejected for incorrect email')
-          reject('Incorrect login')
+          console.log('rejected for incorrect email');
+          reject('Incorrect login');
         }
       })
-      .catch((err)=>{
-          console.log(err)
-          reject('Database error')
-      })
-  })
-
+      .catch((err) => {
+        console.log(err);
+        reject('Database error');
+      });
+  });
 }
 
-function increaseLockCount(user){
-
+function increaseLockCount (user) {
   return new Promise((resolve, reject) => {
-    var loginCount=user.bad_logins||1
-    loginCount++
+    let loginCount = user.bad_logins || 1;
+    loginCount++;
 
-    if(loginCount ==10){
-//      console.log('set login count to '+loginCount)
-      var resetGuid=Helpers.createGUID()
-
+    if (loginCount === 10) {
+      const resetGuid = uuidv4();
 
       var firstname;
       try {
-        firstname = JSON.parse(user.user_data).firstname
+        firstname = JSON.parse(user.user_data).firstname;
       } catch (e) {
-        firstname = 'User'
+        console.log(`failed to get username from JSON, settings to 'User'`);
+        firstname = 'User';
       }
       Notify.sendPasswordLockEmail({
         email: user.user_name,
@@ -220,113 +199,51 @@ function increaseLockCount(user){
 //        console.log('sent email with notify')
       }).catch((res) => {
 //        console.log('could not send email with notify')
-      }).then(()=>{
-        var queryParams = [user.user_id,loginCount,resetGuid]
-        var query = `update idm.users set password='VOID',reset_guid=$3,bad_logins=$2 where user_id=$1`
-          DB.query(query, queryParams).then((res)=>{
-            resolve()
-          }).catch((err)=>{
-            console.log(err)
-            reject()
+      }).then(() => {
+        const queryParams = [user.user_id, loginCount, resetGuid];
+        const query = `update idm.users set password='VOID',reset_guid=$3,bad_logins=$2 where user_id=$1`;
+        DB.query(query, queryParams)
+          .then((res) => {
+            resolve();
           })
+          .catch((err) => {
+            console.log(err);
+            reject();
+          });
       });
-
-
-      //notify!
-    } else if (loginCount > 10){
-      var queryParams = [user.user_id,loginCount]
-      var query = `update idm.users set bad_logins=$2 where user_id=$1`
-        DB.query(query, queryParams).then((res)=>{
-          resolve()
-        }).catch(()=>{
-          reject()
-        })
-  } else {
-    var query = `update idm.users set bad_logins=$2 where user_id=$1`
-      var queryParams = [user.user_id,loginCount]
-    DB.query(query, queryParams).then((res)=>{
-      resolve()
-    }).catch(()=>{
-      reject()
-    })
-  }
-
-
+    } else if (loginCount > 10) {
+      const queryParams = [user.user_id, loginCount];
+      const query = `update idm.users set bad_logins=$2 where user_id=$1`;
+      DB.query(query, queryParams).then((res) => {
+        resolve();
+      }).catch(() => {
+        reject();
+      });
+    } else {
+      var query = `update idm.users set bad_logins=$2 where user_id=$1`;
+      var queryParams = [user.user_id, loginCount];
+      DB.query(query, queryParams)
+        .then((res) => {
+          resolve();
+        }).catch(() => {
+          reject();
+        });
+    }
   });
 }
 
-function resetLockCount(user){
+function resetLockCount (user) {
   return new Promise((resolve, reject) => {
-    var query = `update idm.users set bad_logins=0,last_login=clock_timestamp()  where user_id=$1`
-    var queryParams = [user.user_id]
-    DB.query(query, queryParams).then((res)=>{
-      resolve()
-    }).catch(()=>{
-      reject()
-    })
+    var query = `update idm.users set bad_logins=0,last_login=clock_timestamp()  where user_id=$1`;
+    var queryParams = [user.user_id];
+    DB.query(query, queryParams)
+      .then(res => resolve())
+      .catch(() => reject());
   });
 }
-
-
-// function getUser(request, reply) {
-//   // Find user by numeric ID
-//   if(typeof(request.params.user_id) === 'number') {
-//     var query = `select * from idm.users where user_id=$1`
-//   }
-//   // Find user by email address
-//   else {
-//       var query = `select * from idm.users where user_name=$1`
-//   }
-//
-//   var queryParams = [request.params.user_id]
-//   DB.query(query, queryParams)
-//     .then((res) => {
-//       if (res.err) {
-//         reply(res.err).code(500)
-//       } else if (!res.data || !res.data[0]) {
-//         reply({
-//           err: 'User not found'
-//         }).code(404)
-//       } else {
-//         var user = res.data[0];
-//         delete user.password
-//         reply(user)
-//       }
-//
-//     })
-// }
-
-
-
-
-//
-// function getUsers(request, reply) {
-//
-//   let query = `select * from idm.users`;
-//   const queryParams = [];
-//
-//   if(request.query.filter) {
-//     const filter = JSON.parse(request.query.filter);
-//
-//     if(filter.reset_guid) {
-//       queryParams.push(filter.reset_guid);
-//       query += ` WHERE reset_guid=$1`;
-//     }
-//   }
-//
-//   DB.query(query, queryParams)
-//     .then((res) => {
-//       if (res.err) {
-//         reply({error : res.err, data : null}).code(500)
-//       }
-//       else {
-//         reply(res.data);
-//       }
-//     })
-// }
 
 module.exports = {
   loginUser,
   loginAdminUser,
   reset
-}
+};
--- src/routes/idm.js
+++ src/routes/idm.js
@@ -1,13 +1,8 @@
-/*
-
-API operations only - NO UI
-
-*/
-
-const IDM = require('../lib/idm')
-const version = '1.0'
-const Joi = require('joi')
-const {pool} = require('../lib/connectors/db');
+/* API operations only - NO UI */
+const Joi = require('joi');
+const IDM = require('../lib/idm');
+const { version } = require('../../config');
+const { pool } = require('../lib/connectors/db');
 const apiConfig = {
   pool,
   version
@@ -17,54 +12,65 @@ const UsersController = require('../controllers/user')(apiConfig);
 const KpiApi = require('../controllers/kpi-reports.js')(apiConfig);
 
 module.exports = [
-  { method: 'GET', path: '/status', handler: function(request,reply){return reply('ok').code(200)}, config:{
-    auth: false,
-    description: 'Status placeholder'
-  }},
-
-
-  ...UsersController.getRoutes(),
-
-
-  { method: 'PATCH', path: '/idm/' + version + '/reset/{email}', handler: IDM.reset, config: {
-    validate : {
-      params : {
-        email : Joi.string().email().required()
-      },
-      query : {
-        mode : Joi.string().valid('reset', 'new', 'existing', 'sharing'),
-        sender : Joi.string().email()
-      }
-    },
-    description : 'Reset user password and send one of a selection of reset emails'
-  }},
-
-
-
-  { method: 'POST', path: '/idm/' + version + '/user/login',   handler: IDM.loginUser, config:{
-    description: 'Login for non-admin users, responds with user details',
-    validate : {
-      payload : {
-        user_name : Joi.string().required(),
-        password : Joi.string().required()
-      }
-    }
-  }},
-
-  { method: 'POST', path: '/idm/' + version + '/user/loginAdmin',   handler: IDM.loginAdminUser , config:{
-    description:'Login for admin users, responds with user details',
-    validate : {
-      payload : {
-        user_name : Joi.string().required(),
-        password : Joi.string().required()
-      }
-    }
-  }},
-
   {
-      method: '*',
-      path: '/', // catch-all path
-      handler: function(request,reply){reply().code(404)}
+    method: 'GET',
+    path: '/status',
+    handler: () => 'ok',
+    options: {
+      auth: false,
+      description: 'Status placeholder'
+    }
+  },
+  ...UsersController.getRoutes(),
+  {
+    method: 'PATCH',
+    path: '/idm/' + version + '/reset/{email}',
+    handler: IDM.reset,
+    options: {
+      validate: {
+        params: {
+          email: Joi.string().email().required()
+        },
+        query: {
+          mode: Joi.string().valid('reset', 'new', 'existing', 'sharing'),
+          sender: Joi.string().email()
+        }
+      },
+      description: 'Reset user password and send one of a selection of reset emails'
+    }
+  },
+  {
+    method: 'POST',
+    path: '/idm/' + version + '/user/login',
+    handler: IDM.loginUser,
+    options: {
+      description: 'Login for non-admin users, responds with user details',
+      validate: {
+        payload: {
+          user_name: Joi.string().required(),
+          password: Joi.string().required()
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/idm/' + version + '/user/loginAdmin',
+    handler: IDM.loginAdminUser,
+    options: {
+      description: 'Login for admin users, responds with user details',
+      validate: {
+        payload: {
+          user_name: Joi.string().required(),
+          password: Joi.string().required()
+        }
+      }
+    }
+  },
+  {
+    method: '*',
+    path: '/', // catch-all path
+    handler: (request, h) => h.code(404)
   },
   KpiApi.findManyRoute()
-]
+];
