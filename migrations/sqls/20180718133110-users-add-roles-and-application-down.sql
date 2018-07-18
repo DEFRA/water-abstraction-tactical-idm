d4affa0104323257134698fb697b260632481039
Mark Swaffer <mdswaffer@gmail.com>
2018-07-18 22:50:01 +0100

    Extracts unique constraint into separate migration

--- migrations/sqls/20180718133110-users-add-roles-and-application-down.sql
+++ migrations/sqls/20180718133110-users-add-roles-and-application-down.sql
@@ -11,8 +11,3 @@ alter table idm.users
   drop column date_updated;
 
 drop type application_name;
-
--- Remove the application from the unique constraint.
-alter table idm.users drop constraint if exists user_name_unique;
-alter table idm.users
-    add constraint user_name_unique unique (user_name)
