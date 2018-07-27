2254e853637e9ef02c661d656d5c8f1f871fad58
Mark Swaffer <mdswaffer@gmail.com>
2018-07-27 10:19:08 +0100

    WATER-1272: Rewrites migrations
    Changes the migration so that if the CRM schema does not exist, then the
    pull of data from the `crm.entity` and `crm.entity_roles` tables is not
    performed.

--- migrations/sqls/20180724104315-users-add-external-id-up.sql
+++ migrations/sqls/20180724104315-users-add-external-id-up.sql
@@ -1,25 +1,46 @@
 alter table idm.users
   add column external_id character varying;
 
-update idm.users
-set external_id = sub.entity_id,
-  date_updated = now()
-from (
-  select entity_id, entity_nm
-  from crm.entity
-  where entity_type = 'individual'
-) as sub
-where idm.users.user_name = sub.entity_nm;
+drop function if exists idm.updateUserRoles();
 
-update idm.users
-set role = '{ "scopes": ["internal"] }'::jsonb
-from (
-  select entity_id
-  from crm.entity_roles
-  where role = 'admin'
-) sub
-where idm.users.external_id = sub.entity_id
-and idm.users.application = 'water_vml';
+create function idm.updateUserRoles() returns void as 
+$$
+
+begin	
+
+  if exists (
+    select schema_name
+    from information_schema.schemata
+    where schema_name = 'crm'
+  )
+  then 
+    update idm.users
+    set external_id = sub.entity_id,
+      date_updated = now()
+    from (
+      select entity_id, entity_nm
+      from crm.entity
+      where entity_type = 'individual'
+    ) as sub
+    where idm.users.user_name = sub.entity_nm;
+
+    update idm.users
+    set role = '{ "scopes": ["internal"] }'::jsonb
+    from (
+      select entity_id
+      from crm.entity_roles
+      where role = 'admin'
+    ) sub
+    where idm.users.external_id = sub.entity_id
+    and idm.users.application = 'water_vml';
+  end if;
+
+end;
+$$ LANGUAGE plpgsql;
+
+select idm.updateuserroles();
+
+drop function idm.updateUserRoles();
 
 update idm.users
 set role = '{ "scopes": ["external"] }'::jsonb
