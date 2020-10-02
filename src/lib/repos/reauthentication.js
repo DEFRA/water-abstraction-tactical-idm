8cc6f56402a3f942d53f9cbc63134f8538c23809
Tim <timtamimi@ip-192-168-68-103.eu-west-2.compute.internal>
2020-10-02 10:04:53 +0100

    Removed some unnecessary spaces and SQL terminators

--- src/lib/repos/reauthentication.js
+++ src/lib/repos/reauthentication.js
@@ -10,14 +10,9 @@ class ReauthenticationRepository {
    * @return {Promise} resolves with idm.reauthentication record
    */
   async findByUserId (userId) {
-    const query = `
-      INSERT INTO idm.reauthentication
+    const query = `INSERT INTO idm.reauthentication
         (reauthentication_id, user_id, reference_date, date_created, date_updated, attempts)
-        VALUES (gen_random_uuid(), $1, CURRENT_DATE, NOW(), NOW(), 0)
-      ON CONFLICT (user_id, reference_date)
-        DO UPDATE SET date_updated=NOW(), attempts=reauthentication.attempts+1
-      RETURNING *
-      `;
+        VALUES (gen_random_uuid(), $1, CURRENT_DATE, NOW(), NOW(), 0) ON CONFLICT (user_id, reference_date) DO UPDATE SET date_updated=NOW(), attempts=reauthentication.attempts+1 RETURNING *;`;
     const params = [userId];
     const { rows: [record] } = await this.connection.query(query, params);
     return record;
@@ -28,11 +23,9 @@ class ReauthenticationRepository {
    * @param  {Number}  userId - the user_id from idm.users
    */
   resetAttemptCounter (userId) {
-    const query = `
-      UPDATE idm.reauthentication
+    const query = `UPDATE idm.reauthentication
         SET attempts=0, date_updated=NOW()
-        WHERE user_id=$1 AND reference_date=CURRENT_DATE
-    `;
+        WHERE user_id=$1 AND reference_date=CURRENT_DATE;`;
     const params = [userId];
     return this.connection.query(query, params);
   }
