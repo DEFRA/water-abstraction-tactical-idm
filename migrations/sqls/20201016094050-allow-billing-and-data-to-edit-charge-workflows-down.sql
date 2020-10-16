dc32b0e0477fe16c019ec5c35f9ec64fa54d8e2d
Tim <timtamimi@Tims-MacBook-Pro.local>
2020-10-16 10:58:13 +0100

    Tweak suggested in PR - on down, only remove rows where application is water_admin

--- migrations/sqls/20201016094050-allow-billing-and-data-to-edit-charge-workflows-down.sql
+++ migrations/sqls/20201016094050-allow-billing-and-data-to-edit-charge-workflows-down.sql
@@ -4,6 +4,7 @@ where group_id in (
   select group_id
   from idm.groups
   where groups."group" = 'billing_and_data'
+  and groups.application='water_admin'
 ) and role_id in (
     select role_id from idm.roles where role ='charge_version_workflow_editor'
 )
