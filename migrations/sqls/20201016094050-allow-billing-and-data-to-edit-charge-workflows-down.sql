b22c7fc734802cbd822a8dfd6f2f20e994117d6a
Tim <timtamimi@Tims-MacBook-Pro.local>
2020-10-16 10:45:16 +0100

    Add the charge_version_workflow_editor role permission to the billing and data role

---
+++ migrations/sqls/20201016094050-allow-billing-and-data-to-edit-charge-workflows-down.sql
@@ -0,0 +1,9 @@
+-- Remove the agreements role from the groups
+delete from idm.group_roles
+where group_id in (
+  select group_id
+  from idm.groups
+  where groups."group" = 'billing_and_data'
+) and role_id in (
+    select role_id from idm.roles where role ='charge_version_workflow_editor'
+)
