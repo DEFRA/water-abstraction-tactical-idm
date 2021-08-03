0dc09304e49a6d50c0885e1bdb0ba861448519e6
Tim <tim@tamimi.dev>
2021-08-03 09:16:03 +0100

    Update 20171201101811-idm-up.sql

--- migrations/sqls/20171201101811-idm-up.sql
+++ migrations/sqls/20171201101811-idm-up.sql
@@ -20,7 +20,7 @@ set schema 'idm';
 -- ----------------------------
 -- Sequence structure for users_user_id_seq
 -- ----------------------------
-CREATE SEQUENCE if not exists "users_user_id_seq"
+CREATE SEQUENCE if not exists idm."users_user_id_seq"
 INCREMENT 1
 MINVALUE  1
 MAXVALUE 9223372036854775807
